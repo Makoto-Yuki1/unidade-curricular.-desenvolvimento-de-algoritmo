5d3518cfebe898bdbd1a95d3bc68827671525b5e
Makoto-Yuki1 <dragonoinvencivel@gmail.com>
2026-04-08 08:32:35 -0300

    Add files via upload

---
+++ atividade especial OBI/atividade2.py
@@ -0,0 +1,11 @@
+quantidade=int(input("quantas pessoas estão infectadas no dia 0?"))
+dias= int(input("quantos dias de pandemnia?"))
+pessoasinfectadasnototal= quantidade * (dias + 1)
+
+print(pessoasinfectadasnototal)
+
+
+
+
+
+
