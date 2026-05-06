5af6fa75be0853669be7b568795136aabbbd168e
Makoto-Yuki1 <dragonoinvencivel@gmail.com>
2026-05-06 17:26:16 -0300

    Add files via upload

--- atividade6.py
+++ atividade6.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/produto/<nome>/<float:preco>')
+def produto(nome, preco):
+    return 'o produto é:{nome}, e seu preço é: {preco}|'
+   
 
 
 
