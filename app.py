77fadc56318d72e158c4530219f8ba48f5dfde8a
Makoto-Yuki1 <dragonoinvencivel@gmail.com>
2026-05-21 08:40:52 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,22 @@
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
+@app.route('/')
+
 @app.route('/index')
 def index():
-   return render_template('jjk.html')
+    return render_template('login.html')
 
-@app.route('/contato')
-def contato():
-    nome = "Gaby"
-    return render_template('jjk.html', title = 'página inicial',nome = nome )
+@app.route('/autenticar', methods = ['GET'])
+def autenticar():
+     nome= request.args.get('nome')
+     curso= request.args.get('nome')
+     cidade = request.args.get('cidade')
+     idade = request.args.get('idade')
+     return "nome {} e curso {} e cidade {} e idade {}".format(nome, curso, cidade, idade)
 
-
-@app.route('/usuario')
-def usuario():
- usuario = {'nome', 'Gaby', 'email:' 'cortezyasmin@gmail.com'}
- return render_template('jjk.html', title = 'página inicial', usuario = usuario )
-
-@app.route('/dados', defaults={"nome":"usuário comum"})
-@app.route('/dados/<nome>')
-def dados(nome): 
-  return f'olá, {nome}!'
-
-
-@app.route('/semestre/<int:x>')
-def semestre(x):
-   return 'estamos no semestre' + str(x)
-
-@app.route('/pagamento/<float:valor>')
-def pagamento(valor):
- return 'você pagou:' + str(valor)
-
-@app.route('/arearestrita/<id>')
-def arearestrita(valor):
- if (valor == 1):
-  return 'você está livre' 
- else:
-  return 'você foi bloqueado'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
