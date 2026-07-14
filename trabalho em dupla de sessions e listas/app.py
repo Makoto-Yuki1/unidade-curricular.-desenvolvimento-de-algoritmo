62e7add5350963dc135e69f974804a0dbde73674
Makoto-Yuki1 <dragonoinvencivel@gmail.com>
2026-07-14 00:25:48 -0300

    Refactor app.py for session handling and routes

--- trabalho em dupla de sessions e listas/app.py
+++ trabalho em dupla de sessions e listas/app.py
@@ -10,19 +10,6 @@ from flask import (
 
 
 app = Flask(__name__)
-
-@app.route('/adicionar', methods=['GET', 'POST'])
-def adicionar(): 
-    tarefas = [
-        {'id': 1, 'nome': 'brócolis','calorias': 10.00},
-        {'id': 2, 'nome': 'picanha','calorias': 280.00},
-        {'id': 3, 'nome': 'miojo SABOORRRR picanha','calorias': 150.00},
-        {'id': 4, 'nome': 'macarrão prego','calorias': 200.00},
-        {'id': 5, 'nome': 'feijão preto', 'calorias': 250.00},
-        {'id': 6, 'nome': 'bixcoito maizena','calorias': 300.00}
-    ]
-    return render_template('index.html', tarefas=tarefas)
-
 @app.route('/adicionar', methods=['GET', 'POST'])
 def tarefas():
     if request.method == 'POST':
